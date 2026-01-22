162c313b1f3bf110f05e7b0196fe4f97cea48bdc
Shad Sharma <shadanan@gmail.com>
2026-01-21 23:04:18 -0800

    Initial Commit

---
+++ SSHadow/SSHadowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSHadowApp.swift
+//  SSHadow
+//
+//  Created by Shad Sharma on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct SSHadowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
