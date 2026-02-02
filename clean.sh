ba075bb1e1012f6f69b7fc0df3130787f805281c
Shad Sharma <shadanan@gmail.com>
2026-02-02 12:00:17 -0800

    fix: file provider name (#15)

--- clean.sh
+++ clean.sh
@@ -4,3 +4,4 @@ rm -rf ~/Library/Application\ Support/FileProvider/com.kosolabs.SSHadow*
 rm -rf ~/Library/Containers/com.kosolabs.SSHadow*
 rm -rf ~/Library/Developer/Xcode/DerivedData/SSHadow*
 rm -rf ~/Library/Group\ Containers/group.com.kosolabs.SSHadow*
+rm -rf ~/Library/Developer/Xcode/Archives/*/SSHadow*
