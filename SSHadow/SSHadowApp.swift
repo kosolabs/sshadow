a3da227c42a4e53965cbbcbdfbc5c955619fb851
Shad Sharma <shadanan@gmail.com>
2026-02-07 18:22:41 -0800

    test: check if deleting UI tests fixes shared tests

--- SSHadow/SSHadowApp.swift
+++ SSHadow/SSHadowApp.swift
@@ -1,16 +1,38 @@
+import OSLog
 import SSHadowShared
 import SwiftData
 import SwiftUI
+
+private let logger = Logger(
+    subsystem: Bundle.main.bundleIdentifier!,
+    category: "SSHadowApp"
+)
 
 @main
 struct SSHadowApp: App {
     var body: some Scene {
         WindowGroup {
-            ConnectionProfileListView()
+            if isUnitTesting {
+                Text("Unit Testing")
+            } else {
+                ConnectionProfileListView()
+            }
         }
         .modelContainer(
             for: ConnectionProfile.self,
-            inMemory: ProcessInfo.processInfo.arguments.contains("-uiTesting")
+            inMemory: isUITesting || isUnitTesting
         )
+    }
+
+    private var isUITesting: Bool {
+        ProcessInfo.processInfo.arguments.contains("-uiTesting")
+    }
+
+    private var isUnitTesting: Bool {
+        let result =
+            ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"]
+            != nil
+        logger.debug("isUnitTesting: \(result)")
+        return result
     }
 }
