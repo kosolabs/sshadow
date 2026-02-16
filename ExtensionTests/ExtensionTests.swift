0ffd42587f216be1076e2c360d067811ef774580
Shad Sharma <shadanan@gmail.com>
2026-02-15 18:46:52 -0800

    test: use temporary folder for unit tests (#33)

--- ExtensionTests/ExtensionTests.swift
+++ ExtensionTests/ExtensionTests.swift
@@ -5,18 +5,43 @@ import Testing
 @testable import Extension
 
 struct ExtensionTests {
-    @Test func initializeValidConfigSucceeds() throws {
-        let uuid = UUID()
+    private func getExtension(id: UUID = UUID()) throws -> Extension {
         let domain = NSFileProviderDomain(
-            identifier: NSFileProviderDomainIdentifier(rawValue: uuid.uuidString),
+            identifier: NSFileProviderDomainIdentifier(
+                rawValue: id.uuidString
+            ),
             displayName: "test"
         )
-        let userInfo = try TestData.getUserInfo(id: uuid)
+        let userInfo = try TestData.getUserInfo(id: id)
         domain.userInfo = try userInfo.toDictionary()
-        
-        let expectedConfig = try TestData.getConnectionConfig(id: uuid)
-        
-        let actualConfig = Extension(domain: domain).config
+        return Extension(domain: domain)
+    }
+
+    @Test func initializeValidConfigSucceeds() throws {
+        let ext = try getExtension()
+        let actualConfig = ext.config
+
+        let id = try #require(actualConfig?.id)
+        let expectedConfig = try TestData.getConnectionConfig(id: id)
+
         #expect(actualConfig == expectedConfig)
+    }
+
+    @Test func itemSucceeds() async throws {
+        let ext = try getExtension()
+        
+        let path = "test-item-succeeds/item.txt"
+        let contents = "Hello, World!"
+        try TestData.createTestFile(path: path, contents: contents)
+
+        let progress = Progress()
+        let item = try await ext.item(
+            for: .rootContainer.child(name: path),
+            request: NSFileProviderRequest(),
+            progress: progress
+        )
+
+        #expect(item.filename == "item.txt")
+        #expect(item.documentSize??.intValue == contents.count)
     }
 }
