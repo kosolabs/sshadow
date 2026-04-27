5346ef5307639d7bb039126f87f267b86e8f0cb6
Shad Sharma <shadanan@gmail.com>
2026-04-27 12:12:50 -0700

    refactor: delete the old agent target

--- Common/SSHadow.swift
+++ Common/SSHadow.swift
@@ -4,7 +4,6 @@ public enum SSHadow {
     public static let bundleId =
         Bundle.main.bundleIdentifier ?? "com.kosolabs.SSHadow"
     public static let appGroup = "group.com.kosolabs.SSHadow"
-    public static let agentServiceName = "\(appGroup).Agent"
     public static let appServiceName = "\(appGroup).App"
     public static let groupUrl = FileManager.default.containerURL(
         forSecurityApplicationGroupIdentifier: appGroup
