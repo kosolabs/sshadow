9bfa63a530dffbea675a8a8877d02d20c7359fb9
Shad Sharma <shadanan@gmail.com>
2026-07-21 16:30:47 -0700

    refactor: remove legacy app XPCListener service

--- Common/SSHadow.swift
+++ Common/SSHadow.swift
@@ -8,7 +8,6 @@ public enum SSHadow {
     public static let bundleId =
         Bundle.main.bundleIdentifier ?? "com.kosolabs.SSHadow"
     public static let appGroup = "group.com.kosolabs.SSHadow"
-    public static let appServiceName = "\(appGroup).App"
     public static let extensionServiceName = NSFileProviderServiceName(
         "\(appGroup).Extension"
     )
